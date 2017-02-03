a229a69c3f3b49e05b66936837bfad331421eab1
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-02-04 02:15:04 +0800

    Updated compile script with chapter 1

--- MARKDOWN/compile2tex.sh
+++ MARKDOWN/compile2tex.sh
@@ -1,1 +1,4 @@
-pandoc -t latex -o ../_pdf/chapter1.tex --smart 01.md 01_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm1.tex --smart 01.md 01_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm2.tex --smart 02.md 02_fn.md --top-level-division=chapter
+
+pandoc -t latex -o ../_pdf/chapter1.tex --smart 03.md 03_fn.md --top-level-division=chapter
