e33e62bdad5c8b2c9790bb71c6275cadf8d92d54
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-02-05 07:14:01 +0800

    Having some Git issues, updated relevant files (again?)

--- MARKDOWN/compile2tex.sh
+++ MARKDOWN/compile2tex.sh
@@ -1,8 +1,12 @@
 # Frontmatter
-pandoc -t latex -o ../_pdf/fm1.tex --smart 01.md 01_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/fm2.tex --smart 02.md 02_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm1.tex 01.md 01_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm2.tex 02.md 02_fn.md --top-level-division=chapter
 
 # Main Matter
-pandoc -t latex -o ../_pdf/chapter1.tex --smart 03.md 03_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter2.tex --smart 04.md 04_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter3.tex --smart 05.md 05_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter1.tex 03.md 03_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter2.tex 04.md 04_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter3.tex 05.md 05_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter4.tex 06.md 06_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter5.tex 07.md 07_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter6.tex 08.md 08_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter7.tex 09.md 09_fn.md --top-level-division=chapter
