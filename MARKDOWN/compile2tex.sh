f960c39520f7bd0a03ffb380550978ed0c2b7980
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-02-16 07:01:56 +0800

    Miscellaneous edits

--- MARKDOWN/compile2tex.sh
+++ MARKDOWN/compile2tex.sh
@@ -5,7 +5,7 @@ pandoc -t latex -o ../_pdf/fm2.tex 02.md 02_fn.md --top-level-division=chapter
 # Main Matter
 pandoc -t latex -o ../_pdf/chapter1.tex 03.md 03_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter2.tex 04.md 04_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter3.tex 05.md 05_fn.md --top-level-division=chapter
+# pandoc -t latex -o ../_pdf/chapter3.tex 05.md 05_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter4.tex 06.md 06_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter5.tex 07.md 07_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter6.tex 08.md 08_fn.md --top-level-division=chapter
@@ -15,4 +15,5 @@ pandoc -t latex -o ../_pdf/chapter9.tex 11.md 11_fn.md --top-level-division=chap
 pandoc -t latex -o ../_pdf/chapter10.tex 12.md 12_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter11.tex 13.md 13_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter12.tex 14.md 14_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter13.tex 15.md 15_fn.md --top-level-division=chapter
 
