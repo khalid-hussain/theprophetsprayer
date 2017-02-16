c5d8ba47bee08404754064d1fe9ca7f2fca31779
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-02-17 02:29:25 +0800

    Updated HTML skeleton along with compilation script and first prototype

--- MARKDOWN/compile2html.sh
+++ MARKDOWN/compile2html.sh
@@ -1,8 +1,53 @@
-pandoc -t html5 -o ../_html/fm1.html metadata.yaml 01.md 01_fn.md 02.md 02_fn.md \
+pandoc -t html5 -o ../_html/index.html metadata.yaml toc_short.md \
+-N \
 -c css/normalize.css \
 -c css/skeleton.css \
--c css/index.css \
+-c css/toc.css \
 -c css/bigfoot-default.css \
 -B ../_html/header.html \
--A ../_html/footer.html \
---toc
+-A ../_html/footer.html
+
+pandoc -t html5 -o ../_html/sections/introduction.html 01.md 01_fn.md 02.md 02_fn.md \
+-N --toc \
+-c ../css/normalize.css \
+-c ../css/skeleton.css \
+-c ../css/index.css \
+-c ../css/bigfoot-default.css \
+-B ../_html/header.html \
+-A ../_html/footer.html
+
+pandoc -t html5 -o ../_html/sections/facing_the_kabah.html 03.md 03_fn.md \
+-N --toc \
+-c ../css/normalize.css \
+-c ../css/skeleton.css \
+-c ../css/index.css \
+-c ../css/bigfoot-default.css \
+-B ../_html/header.html \
+-A ../_html/footer.html
+
+pandoc -t html5 -o ../_html/sections/standing_in_prayer.html 04.md 04_fn.md \
+-N --toc \
+-c ../css/normalize.css \
+-c ../css/skeleton.css \
+-c ../css/index.css \
+-c ../css/bigfoot-default.css \
+-B ../_html/header.html \
+-A ../_html/footer.html
+
+pandoc -t html5 -o ../_html/sections/intention_and_takbir.html 06.md 06_fn.md 07.md 07_fn.md \
+-N --toc \
+-c ../css/normalize.css \
+-c ../css/skeleton.css \
+-c ../css/index.css \
+-c ../css/bigfoot-default.css \
+-B ../_html/header.html \
+-A ../_html/footer.html
+
+pandoc -t html5 -o ../_html/sections/opening_supplications.html 08.md 08_fn.md \
+-N --toc \
+-c ../css/normalize.css \
+-c ../css/skeleton.css \
+-c ../css/index.css \
+-c ../css/bigfoot-default.css \
+-B ../_html/header.html \
+-A ../_html/footer.html
---
+++ MARKDOWN/toc_short.md
@@ -0,0 +1,52 @@
+
+<!-- Frontmatter -->
+
+# [A brief biography of the author] {.unnumbered}
+# [Introduction] {.unnumbered}
+
+---
+
+<!-- Mainmatter -->
+
+# [Facing the Kaʿbah]
+# [Standing in Prayer]
+# [Intention and Takbīr]
+# [Opening Supplications (Duʿāʾ)]
+# [Recitation]
+# [The Rukūʿ (Bowing)]
+# [The Sujūd (Prostration)]
+# [The Second Rakʿah]
+# [The First Tashaddud]
+# [The Final Tashaddud]
+# [The Taslīm]
+
+---
+
+<!-- Backmatter -->
+
+# [Addendum] {.unnumbered}
+# [Appendices] {.unnumbered}
+# [Glossary] {.unnumbered}
+
+<!-- Links -->
+
+[A brief biography of the author]: sections/biography.html
+[Introduction]: sections/introduction.html
+
+[Facing the Kaʿbah]: sections/facing_the_kabah.html
+[Standing in Prayer]: sections/standing_in_prayer.html
+[Intention and Takbīr]: sections/intention_and_takbir.html
+[Opening Supplications (Duʿāʾ)]: sections/opening_supplications.html
+[Recitation]: sections/recitation.html
+[The Rukūʿ (Bowing)]: sections/ruku_bowing.html
+[The Sujūd (Prostration)]: sections/sujud_prostration.html
+[The Second Rakʿah]: sections/second_rakah.html
+[The First Tashaddud]: sections/first_tashaddud.html
+[The Final Tashaddud]: sections/second_tashaddud.html
+[The Taslīm]: sections/taslim.html
+
+[Addendum]: sections/addendum.html
+[Appendices]: sections/appendices.html
+[Glossary]: sections/glossary.html
+
+
