cd67bf8875cd48a06d867e2c63132621150ebe25
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-07-12 11:48:09 +0800

    Updated compile script and main LaTeX file to comply with refactored chapter files

--- MARKDOWN/compile2tex.sh
+++ MARKDOWN/compile2tex.sh
@@ -1,20 +1,19 @@
 # Frontmatter
-pandoc -t latex -o ../_pdf/fm1.tex 01.md 01_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/fm2.tex 02.md 02_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm1.tex _fm1.md _fm1_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/fm2.tex _fm2.md _fm2_fn.md --top-level-division=chapter
 
 # Main Matter
-pandoc -t latex -o ../_pdf/chapter1.tex 03.md 03_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter2.tex 04.md 04_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter4.tex 06.md 07.md 06_fn.md 07_fn.md --top-level-division=chapter
-# pandoc -t latex -o ../_pdf/chapter5.tex 07.md 07_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter6.tex 08.md 08_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter7.tex 09.md 09_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter8.tex 10.md 10_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter9.tex 11.md 11_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter10.tex 12.md 12_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter11.tex 13.md 13_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter12.tex 14.md 14_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter13.tex 15.md 15_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter1.tex 01.md 01_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter2.tex 02.md 02_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter3.tex 03.md 03_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter4.tex 04.md 04_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter5.tex 05.md 05_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter6.tex 06.md 06_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter7.tex 07.md 07_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter8.tex 08.md 08_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter9.tex 09.md 09_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter10.tex 10.md 10_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter11.tex 11.md 11_fn.md --top-level-division=chapter
 
 # Back matter
 pandoc -t latex -o ../_pdf/appendix1.tex appendices/appendix_1.md --top-level-division=chapter
