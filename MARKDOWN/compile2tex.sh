229f27516029b149e6c277e8199363e516656f4d
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-07-12 11:06:44 +0800

    Updated compile script to fix chapter order

--- MARKDOWN/compile2tex.sh
+++ MARKDOWN/compile2tex.sh
@@ -5,8 +5,8 @@ pandoc -t latex -o ../_pdf/fm2.tex 02.md 02_fn.md --top-level-division=chapter
 # Main Matter
 pandoc -t latex -o ../_pdf/chapter1.tex 03.md 03_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter2.tex 04.md 04_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter4.tex 06.md 06_fn.md --top-level-division=chapter
-pandoc -t latex -o ../_pdf/chapter5.tex 07.md 07_fn.md --top-level-division=chapter
+pandoc -t latex -o ../_pdf/chapter4.tex 06.md 07.md 06_fn.md 07_fn.md --top-level-division=chapter
+# pandoc -t latex -o ../_pdf/chapter5.tex 07.md 07_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter6.tex 08.md 08_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter7.tex 09.md 09_fn.md --top-level-division=chapter
 pandoc -t latex -o ../_pdf/chapter8.tex 10.md 10_fn.md --top-level-division=chapter
